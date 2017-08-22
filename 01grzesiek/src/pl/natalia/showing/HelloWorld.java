19057758f9034aeba0581c8cf2e11de9f039824a
grkopiec <grkopiec@gmail.com>
2017-08-22 19:06:41 +0200

    init commit

---
+++ 01grzesiek/src/pl/natalia/showing/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.natalia.showing;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		short numberOne = 100;
+		short numberTwo = 146;
+		int sum = numberOne + numberTwo;
+		System.out.println(sum);
+	}
+}
