16f0dc6e18862008ee42331c665bf85ff77b4f39
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2017-09-05 21:29:13 +0200

    Adds comments and literals examples

---
+++ 002Comments/src/pl/java/Comments.java
@@ -0,0 +1,12 @@
+package pl.java;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 /*System.out.println("hello");
+		System.out.println("world");*/
+		System.out.println("zzzzz"); 
+	}
+
+}
