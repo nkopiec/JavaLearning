1a89e31c9d1f25e69b028a667ef5308fae5902ee
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2017-09-05 21:32:17 +0200

    Improves code

--- 000HelloWorld/src/pl/natalia/showing/HelloWorld.java
+++ 000HelloWorld/src/pl/natalia/showing/HelloWorld.java
@@ -3,7 +3,8 @@ package pl.natalia.showing;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");		short numberOne = 100;
+		System.out.println("Hello world");
+		short numberOne = 100;
 		short numberTwo = 146;
 		int sum = numberOne + numberTwo;
 		System.out.println(sum);	}
---
+++ 003VariablesAndLiterals/src/pl/java/Variables.java
@@ -0,0 +1,9 @@
+package pl.java;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		System.out.println(1000 - 100 - 50 + 200);
+	}
+
+}
