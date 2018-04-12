bb89e1451ec67158f72df6a2087477bf1787ba3c
Natalia Kopiec <natalia.kopiec.98@gmail.com>
2018-04-12 15:20:42 +0200

    save and write dane with file

--- Projects/src/pl/java/MathOperations.java
+++ Projects/src/pl/java/MathOperations.java
@@ -1,11 +1,16 @@
 package pl.java;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
+import static javax.swing.JOptionPane.*;
 import java.util.StringTokenizer;
 
 public class MathOperations {
 
 	public static void main(String[] args) {
-		String normalQuest = "Number1 op Number2",
+		String normalQuest = "Give first number operation and second number",
 				errorQuest = "Flawed data. Try again.\n" + normalQuest,
 				quest = normalQuest,
 				expr = "";
@@ -14,7 +19,7 @@ public class MathOperations {
 			try {
 				StringTokenizer st = new StringTokenizer(expr);
 				int num1 = Integer.parseInt(st.nextToken());
-				String sop = st.nextToken();
+				String sop = st.nextToken(); 
 				int num2 = Integer.parseInt(st.nextToken());
 				
 				if(sop.length() != 1 || st.hasMoreTokens())
@@ -31,17 +36,43 @@ public class MathOperations {
 				quest = errorQuest;
 				continue; 
 			}
-			showInputDialog(null, "Result = " + res);
+			showMessageDialog(null, "Result = " + res);
 			quest = normalQuest;
 			expr = "";
 			
+			
 		}
+		//new BufferedReader(new FileReader(new File("C:/Andrzej/file.txt")));
+		//File file = new File(C:/Andrzej/file.txt);
+		//Scanner odczyt = new Scanner(new File("file0.txt"));
+		//String text = odczyt.nextLine();
+		
+		//zapis danych do pliku
+		String filePath = "C:/Andrzej/file.txt";
+				FileWriter fileWriter = null;
+				try {
+				    fileWriter = new FileWriter(filePath);
+				    fileWriter.write(Integer.toString(res));
+				} finally {
+				    if (fileWriter != null) {
+				        fileWriter.close();
+				    }
+				}
+				
+			//odczyt danych z pliku
+				
+						
+						BufferedReader fileReader = null;
 
-	}
-
-	private static String showInputDialog(String quest, String expr) {
-		// TODO Auto-generated method stub
-		return null;
+						try {
+						    fileReader = new BufferedReader(new FileReader(filePath));
+						    String numberAsString = fileReader.readLine();
+						    res = Integer.parseInt(numberAsString);
+						} finally {
+						    if (fileReader != null) {
+						        fileReader.close();
+						    }
+						}
 	}
 
 }
